412c05f1baac25db2cba72fecbd0e6cef5d03dfc
Claire Kopenhafer <kopenhaf@msu.edu>
2019-02-11 14:25:17 -0500

    Created CLI arg parser

--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 import requests
 import numpy as np
+import argparse
 
+# Are we using MPI? If yes, we must set up MPI runtime first
 try:
     from mpi4py import MPI
     
@@ -32,6 +34,8 @@ try:
 except ImportError: # no mpi4py
     pass # just don't define scatter_work and other MPI comm variables
 
+
+# Helper functions
 def get(path, params=None, fpath=""):
     # make HTTP GET request to path
     headers = {"api-key":"5309619565f744f9248320a886c59bec"}
@@ -64,3 +68,34 @@ def periodic_centering(x, center, boxsize):
         x[x > upper_qrt] -= boxsize
     
     return x - center
+
+# Parse CLI arguments
+parser = argparse.ArgumentParser(
+    description="Specify Illustris version, redshift, and analysis specifications."
+                                )
+parser.add_argument('z', type=float, choices=[0.0, 0.5], action="store",
+                    help='Redshift; only 0.0 or 0.5 currently supported')
+
+parser.add_argument('--parent', action='store_true', dest='parent',
+                    help='Process parent sample')
+
+parser.add_argument('--inst', action='store_true', dest='inst_sfr',
+                    help='Include instantaneous SFR')
+
+parser.add_argument('--dusty', action='store_true', dest='dusty',
+                    help='Include dust in spectra')
+
+parser.add_argument('--tng', action='store_true', dest='tng',
+                    help='Use Illustris TNG instead of original')
+
+args = parser.parse_args()
+
+if !args.tng:
+    url_dset = "http://www.illustris-project.org/api/Illustris-1/"
+    if args.z==0.5:
+        url_snpsht = url_dset + "snapshots/103/"
+        folder = 'z05/'
+    elif args.z==0.0:
+        url_snpsht = url_dset + "snapshots/135/"
+        folder = 'z00/'
+
