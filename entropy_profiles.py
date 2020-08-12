7cc42f4578986105a9b865939f3b1f756d8883ed
Claire Kopenhafer <kopenhaf@msu.edu>
2020-08-12 16:51:14 -0400

    Checking API mass vs M200

--- entropy_profiles.py
+++ entropy_profiles.py
@@ -5,7 +5,7 @@ import readhaloHDF5
 import snapHDF5
 import numpy as np
 import astropy.units as u
-from astropy.constants import m_p, k_B
+from astropy.constants import m_p, k_B, G
 from scipy.stats import binned_statistic_2d
 # prep MPI environnment and import scatter_work(), get(), periodic_centering(),
 # CLI args container, url_dset, url_sbhalos, folder, snapnum, littleh, omegaL/M
@@ -22,8 +22,14 @@ if rank==0:
 
     part_data = np.genfromtxt(folder+"parent_particle_data.csv", names=True)
     sub_list = part_data['id'].astype(np.int32)
+    sat = part_data['satellite'].astype(np.bool)
 
+    np.random.seed(6841325)
+    subset = np.random.choice(sub_list[sat], size=500, replace=False)
+    
     del part_data
+    del sat
+    
 else:
     sub_list = None
                                    
@@ -34,19 +40,24 @@ boxsize = get(url_dset)['boxsize']
 z = args.z
 a0 = 1/(1+z)
 
+H0 = littleh * 100 * u.km/u.s/u.Mpc
+rhocrit = 3*H0**2/(8*np.pi*G)
+rhocrit = rhocrit.to(u.Msun/u.kpc**3)
+
 good_ids = np.where(my_subs > -1)[0]
 my_profiles = {}
 
 #for sub_id in my_subs[good_ids]:
-sub_id = 
+sub_id = 402017
 if True:
     sub = get(url_sbhalos + str(sub_id))
 
     gas = True
     if not args.local:
         # Read particle data
-        gas_file = folder+"gas_cutouts/cutout_{}.hdf5".format(sub_id)
-
+        # gas_file = folder+"gas_cutouts/cutout_{}.hdf5".format(sub_id)
+        gas_file = "/home/claire/cutout_{}.hdf5".format(sub_id)
+        
         # Gas
         try:
             with h5py.File(gas_file) as f:
@@ -55,6 +66,7 @@ if True:
                 mass = f['PartType0']['Masses'][:]
                 inte = f['PartType0']['InternalEnergy'][:]
                 elec = f['PartType0']['ElectronAbundance'][:]
+                dm_coords = f['PartType1']['Coordinates'][:]
         except KeyError:
             gas = False
 
@@ -78,7 +90,8 @@ if True:
             elec = readhaloHDF5.readhalo(args.local, "snap", snapnum,
                                          "NE  ", 0, -1, sub_id, long_ids=True,
                                          double_output=False).astype("float32")
-
+            dm_coords = readhaloHDF5.readhalo(args.local, "snap", snapnum,
+                                              "POS ", 1, -1, sub_id, long_ids=True)
         except AttributeError:
             gas = False
 
@@ -106,9 +119,23 @@ if True:
 
         mass = mass * 1e10 / littleh * u.Msun
 
-
         # TODO calculate r200 and bin K in scaled radial bins
-        
+        dm_x = dm_coords[:,0]
+        dm_y = dm_coords[:,1]
+        dm_z = dm_coords[:,2]
+        dm_x_rel = periodic_centering(dm_x, sub['pos_x'], boxsize) * u.kpc * a0/littleh
+        dm_y_rel = periodic_centering(dm_y, sub['pos_y'], boxsize) * u.kpc * a0/littleh
+        dm_z_rel = periodic_centering(dm_z, sub['pos_z'], boxsize) * u.kpc * a0/littleh
+        dm_r = np.sqrt(dm_x_rel**2 + dm_y_rel**2 + dm_z_rel**2)
+
+        dm_rsort = np.sorted(dm_r)
+        # count DM particles inside a sphere to find density at that radius
+        dm_dens = 7.5e6*u.Msun*np.arange(1,dm_rsort.size+1) / np.power(dm_rsort,3)
+        # pick first particle beyond density cutoff for r200. Density falls with r!
+        r200 = dm_rsort[dm_dens < 200*rhocrit][0]
+
+        per_off = ( mass.sum() - (mass[r < r200].sum()) / mass.sum() ).value * 100
+        print(sub_id, per_off)
 #     else: # no gas
 #         my_profiles[sub_id] = np.nan
 
